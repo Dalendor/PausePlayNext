11b118aef44268322d308cf0985e6b012971b07f
YvenChauvauxBrightest <Yvenchauvaux@hotmail.com>
2024-12-20 14:53:18 +0100

    Created an next track variant of the pauseplay button

--- PausePlay.cpp
+++ PausePlay.cpp
@@ -1,24 +1,36 @@
-﻿#include "framework.h"
+﻿#include <windows.h> // Standard Windows header
 
-#define MAX_LOADSTRING 100
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "kernel32.lib")
 
 
-int APIENTRY _tWinMain(HINSTANCE hInstance,
+
+int APIENTRY WinMain(HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
     LPTSTR    lpCmdLine,
     int       nCmdShow)
 {
+    // Initialize input array
     INPUT inputs[2] = {};
-    ZeroMemory(inputs, sizeof(inputs));
 
+    // Simulate "Next Track" key press
     inputs[0].type = INPUT_KEYBOARD;
-    inputs[0].ki.wVk = VK_MEDIA_PLAY_PAUSE;
+    inputs[0].ki.wVk = VK_MEDIA_NEXT_TRACK;
 
+    // Simulate "Next Track" key release
     inputs[1].type = INPUT_KEYBOARD;
-    inputs[1].ki.wVk = VK_MEDIA_PLAY_PAUSE;
+    inputs[1].ki.wVk = VK_MEDIA_NEXT_TRACK;
     inputs[1].ki.dwFlags = KEYEVENTF_KEYUP;
 
+    // Send the inputs
     UINT uSent = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
 
-    return uSent != ARRAYSIZE(inputs) ? 1 : 0;
+    // Check for errors
+    if (uSent != ARRAYSIZE(inputs)) {
+        DWORD error = GetLastError();
+        // Handle error (logging or displaying an error message could be useful)
+        return 1;
+    }
+
+    return 0; // Success
 }
--- framework.h
+++ framework.h
@@ -1,7 +1,6 @@
 ﻿#pragma once
 
 #include "targetver.h"
-#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <stdlib.h>
 #include <malloc.h>
